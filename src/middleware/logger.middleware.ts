29e8b4a82f291bd51d9e1987e3a541fdc955213b
mahadi40011 <mahadi40011@gmail.com>
2026-06-07 23:34:25 +0600

    modify the logger middleware

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import type { Next, Req, Res } from "../types";
 
 const loggerMiddleware = (req: Req, res: Res, next: Next) => {
-  const log = `Time --> ${Date.now()}, Method --> ${req.method}, URL --> ${req.url}\n`;
+  const log = `Time --> ${new Date().toLocaleString()}, Method --> ${req.method}, URL --> ${req.url}\n`;
   fs.appendFile("logger.txt", log, (error) => {
     console.log(error);
   });
