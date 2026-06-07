a5075b4a0df6593f422212b2e6380d555e8c269c
mahadi40011 <mahadi40011@gmail.com>
2026-06-07 22:42:34 +0600

    write the issues table schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -10,12 +10,30 @@ export const initDB = async () => {
     await pool.query(`
       CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
+
         name VARCHAR(40) NOT NULL,
         email VARCHAR(50) UNIQUE NOT NULL,
         password text NOT NULL,
-        role user_role NOT NULL DEFAULT 'contributor',
+        role VARCHAR(20) NOT NULL DEFAULT 'contributor',
+
         created_at TIMESTAMPTZ DEFAULT NOW(),
         updated_at TIMESTAMPTZ  DEFAULT NOW() 
+      )`);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS issues (
+        id SERIAL PRIMARY KEY,
+        
+        title VARCHAR(150) NOT NULL,
+        description TEXT NOT NULL,
+
+        type VARCHAR(20) NOT NULL,
+        status VARCHAR(20) NOT NULL DEFAULT 'open',
+
+        reporter_id INTEGER NOT NULL,
+
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at TIMESTAMPTZ DEFAULT NOW()
       )`);
 
     console.log("database connected");
