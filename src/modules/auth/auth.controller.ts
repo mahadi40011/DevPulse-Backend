41fa1271b9226385d96fc42caa6fe6070f195944
mahadi40011 <mahadi40011@gmail.com>
2026-06-08 01:10:39 +0600

    complete user authentication service layer

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,12 @@
 import type { Next, Req, Res } from "../../types";
+import { sendResponse } from "../../utils/sendResponse";
 import { authService } from "./auth.service";
 
 const signUpUser = async (req: Req, res: Res, next: Next) => {
   try {
     const result = await authService.createUser(req.body);
-
-    res.status(201).json({
-      success: true,
-      message: "User registered successfully",
-      data: result,
-    });
+    
+    sendResponse(res, 201, "User registered successfully", result);
   } catch (error) {
     next(error);
   }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,13 +1,37 @@
+import { pool } from "../../db";
 import type { IRegisterUserPayload } from "../../types";
 import AppError from "../../utils/appError";
+import bcrypt from "bcryptjs";
 
 const createUser = async (payLoad: IRegisterUserPayload) => {
   const { name, email, password, role } = payLoad;
+
   const allowedRoles = ["maintainer", "contributor"];
 
   if (!allowedRoles.includes(role as string)) {
     throw new AppError("Invalid Role", 400);
   }
+
+  const userExists = await pool.query(`SELECT 1 FROM users WHERE email=$1`, [
+    email,
+  ]);
+
+  if (userExists.rowCount) {
+    throw new AppError("This email is already registered", 409);
+  }
+
+  const hashPassword = await bcrypt.hash(password, 12);
+
+  const result = await pool.query(
+    `INSERT INTO users(name, email, password, role) 
+     VALUES($1, $2, $3, $4)
+     RETURNING *`,
+    [name, email, hashPassword, role],
+  );
+
+  const { password: _, ...user } = result.rows[0];
+  
+  return user;
 };
 
 export const authService = {
