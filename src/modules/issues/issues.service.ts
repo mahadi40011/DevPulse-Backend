8e98746e27696f3d7ace4dd5964db4d51e85c917
mahadi40011 <mahadi40011@gmail.com>
2026-06-08 19:26:24 +0600

    Coding complete of the create issues feature at service layer

--- src/modules/issues/issues.service.ts
+++ src/modules/issues/issues.service.ts
@@ -1,12 +1,30 @@
 import type { JwtPayload } from "jsonwebtoken";
 import type { IIssuesPayload } from "./issues.interface";
+import { pool } from "../../db";
+import AppError from "../../utils/appError";
 
 const createIssuesService = async (
-  payLoad: IIssuesPayload,
+  payload: IIssuesPayload,
   reporterInfo: JwtPayload,
 ) => {
-  console.log("from issues service ", payLoad);
-  return { payLoad, reporterInfo };
+  const { title, description, type } = payload;
+
+  const allowedTypes = ["feature_request", "bug"];
+
+  if (!allowedTypes.includes(type as string)) {
+    throw new AppError("Invalid type", 400);
+  }
+
+  const result = await pool.query(
+    `
+    INSERT INTO issues(title, description, type, reporter_id) 
+    VALUES($1, $2, $3, $4)
+    RETURNING *
+    `,
+    [title, description, type, reporterInfo.id],
+  );
+
+  return result.rows[0];
 };
 
 export const issuesService = {
