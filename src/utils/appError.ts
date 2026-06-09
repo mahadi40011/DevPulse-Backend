9cb992dec27aa6a3a4d54f30014fb24f24b9fa1a
mahadi40011 <mahadi40011@gmail.com>
2026-06-09 21:29:10 +0600

    Modify global error handler and AppError class

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -1,9 +1,11 @@
 class AppError extends Error {
   statusCode: number;
+  data: any;
 
-  constructor(message: string, statusCode: number) {
+  constructor(message: string, statusCode: number, data?: any) {
     super(message);
     this.statusCode = statusCode;
+    this.data = data;
   }
 }
 
